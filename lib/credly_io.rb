04ac740ad379074bfc888ea7f1e74825685fbe09
Brandon Keao <Brandon.Keao@gmail.com>
2017-07-28 18:33:41 -0600

    Gif calls to start for fun dev. Framework for Credly Authentication is next...

--- lib/credly_io.rb
+++ lib/credly_io.rb
@@ -1,11 +1,22 @@
-require "credly_io/version"
+#require "credly_io/version"
 require "httparty"
 
 module CredlyIo
   include HTTParty
   base_uri "animatedgif.me"
 
-  def self.find(id)
-    get("/gifs/#{id}.json")
+  def self.gif_by_id(id)
+    retrieve_url get("/gifs/#{id}.json")
   end
+
+  def self.gif_by_tag(tag)
+    retrieve_url get("/#{tag}.json")
+  end
+
+  private
+
+    def retrieve_url(response)
+      response.parsed_response["url"]
+    end
+
 end
